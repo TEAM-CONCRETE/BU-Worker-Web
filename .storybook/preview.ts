007b430ee882b974fadf8e49e8442325531eb4fe
박승희 <ridia05031@gmail.com>
2025-12-01 01:01:54 +0900

    [#9] chore: storybook 설정

---
+++ .storybook/preview.ts
@@ -0,0 +1,32 @@
+import type { Preview } from "@storybook/react";
+import "../src/app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: "light",
+      values: [
+        {
+          name: "light",
+          value: "#ffffff",
+        },
+        {
+          name: "dark",
+          value: "#0a0a0a",
+        },
+        {
+          name: "worker-neutral-50",
+          value: "#f9fafb",
+        },
+      ],
+    },
+  },
+};
+
+export default preview;
