ee2b4bef0a9092a5e00d06da19d489ed6a6c333d
박승희 <ridia05031@gmail.com>
2025-12-01 02:13:53 +0900

    [#14] feat: 공통 헤더 컴포넌트 구현

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,10 +2,12 @@ export {
   BottomNavigation,
   type BottomNavigationProps,
 } from "@/components/common/BottomNavigation";
+export { Header, type HeaderProps } from "@/components/common/Header";
 export {
   HomeIcon,
   ClockIcon,
   MoneyIcon,
   PersonIcon,
+  ChevronLeftIcon,
   type IconProps,
 } from "@/components/common/icons";
